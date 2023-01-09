2ff50fb8e1f036b7904ce8dc53e210e0cf1bdda2
jedi-99 <96409539+jedi-99@users.noreply.github.com>
2023-01-09 17:04:29 +0530

    Update gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "sample_bucket" {
   name          = "poc-bucket-102030"
   project       = var.project_id
   location      = var.region
-  force_destroy = false
+  force_destroy = true
   versioning {
     enabled = false
   }
