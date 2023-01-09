31155e5dd65f9f60a29ecd1435e9bc3a526c0b45
jedi-99 <96409539+jedi-99@users.noreply.github.com>
2023-01-09 16:54:59 +0530

    Create gcs.tf

---
+++ gcs.tf
@@ -0,0 +1,9 @@
+resource "google_storage_bucket" "sample_bucket" {
+  name          = "poc-bucket-102030"
+  project       = var.project_id
+  location      = var.region
+  force_destroy = false
+  versioning {
+    enabled = false
+  }
+}
